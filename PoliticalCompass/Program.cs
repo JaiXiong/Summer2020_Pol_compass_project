af4fda15819c4dc6f948bc7302aa43259d013724
Jai <jai_1@JAI>
2020-08-10 15:27:14 -0500

    Today we start the project but first we need version control.

---
+++ PoliticalCompass/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PoliticalCompass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
