21acd1552d975599bbc51d2a5856d4f32af57af6
Jai <jai_1@JAI>
2020-08-10 15:27:39 -0500

    Today we get version control on github started.

--- PoliticalCompass/Program.cs
+++ PoliticalCompass/Program.cs
@@ -7,6 +7,7 @@ namespace PoliticalCompass
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Today is the day");
         }
     }
 }
