bcef73614b4012b93e5de8a83614ba4ea98f0e04
Jai <jai_1@JAI>
2020-08-19 23:03:11 -0500

    Implemented + filled in Person class with basic name and standing information.

--- PoliticalCompass/Person.cs
+++ PoliticalCompass/Person.cs
@@ -1,14 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.ExceptionServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace PoliticalCompass
 {
     class Person
     {
+        private string name;
+        private string standing;
         public Person()
         {
+            name = "";
+            standing = "";
+        }
 
+        public Person(string n)
+        {
+            name = n;
+        }
+        
+        public Person(string n, string s)
+        {
+            name = n;
+            standing = s;
         }
     }
 }
